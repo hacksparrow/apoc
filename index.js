93e313540a3dcfa7ae649e9e0d6f79ee251992a6
Hage Yaapa <captain@hacksparrow.com>
2015-12-13 01:13:31 +0530

    update commandline tool

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ global.APOC_GLOBAL_VARS = {}
 let path = require('path')
 let pkg = require(__dirname + '/package.json')
 let program = require('commander')
-let commandlineApoc = require(__dirname + '/lib/commandline-query.js')
+let c = require('chalk')
+let Apoc = require(__dirname + '/lib/apoc-class.js')
 
 program
 .version(pkg.version)
@@ -28,50 +29,54 @@ if (require.main === module) {
     // having an extension helps to ensure we are using the right file
     if (ext === '.acf') {
 
-      let config = require(__dirname + '/lib/config-reader.js')
-      let query = commandlineApoc(cypherFilePath)
+      var apocCmd = new Apoc()
+      apocCmd.query(cypherFilePath)
 
-      query.exec(config).then(function (transactions) {
+      let statementsCount = 0
+      let transactions = apocCmd.transactions
+      transactions.forEach(function (transaction) {
+        statementsCount += transaction.statements.length
+      })
 
-        let statementsCount = 0
-        transactions.forEach(function (transaction) {
-          statementsCount += transaction.length
-        })
+      let statementsCounted = statementsCount > 1 ? 'statements' : 'statement'
 
-        let statementsCounted = statementsCount > 1 ? 'statements' : 'statement'
+      let transactionCount = transactions.length
+      let transactionCounted = transactionCount > 1 ? 'transactions' : 'transaction'
 
-        let transactionCount = transactions.length
-        let transactionCounted = transactionCount > 1 ? 'transactions' : 'transaction'
+      apocCmd.exec().then(function (result) {
 
         if (program.verbose) {
-          console.log('%s >', transactionCounted.toUpperCase())
-          logQuery(query)
-          console.log('RESULT >\n')
-          console.log(JSON.stringify(transactions))
-          console.log('')
+          console.log(c.bold('\n%s >'), transactionCounted.toUpperCase())
+          logQuery(apocCmd.transactions)
+          console.log(c.bold('RESULT >\n'))
+          console.log(JSON.stringify(result))
         }
 
-        console.log('Query executed successfully with %d %s in %d %s',
+        console.log('\nQuery executed successfully with %d %s in %d %s \n',
           statementsCount, statementsCounted,
           transactionCount, transactionCounted)
 
       }, function (fail) {
         if (program.verbose) {
-          logQuery(query)
+          console.log(c.bold('\n%s >'), transactionCounted.toUpperCase())
+          logQuery(apocCmd.transactions)
+          console.log(c.bold.red('FAIL >\n'))
           console.log(fail)
         }
-        else console.log(fail.message)
+        console.log()
+        console.log(fail.message)
+        console.log()
       })
     } else {
-      console.log('File type "%s" not supported', ext)
+      console.log('\n File type "%s" not supported\n', ext)
     }
   }
 } else {
-  module.exports = require(__dirname + '/lib/apoc-module.js')
+  module.exports = Apoc
 }
 
-function logQuery (query) {
+function logQuery(query) {
   console.log()
-  console.log(query.transactions)
+  console.log(query)
   console.log()
 }
